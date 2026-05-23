975e26e90cebbe9b6f1b47efede3f16229d245fb
MANOJ <workwithmonji@gmail.com>
2026-05-23 22:47:17 +0530

    Initial Spring AI - GEMINI Integration

---
+++ src/main/java/com/monji/ai_test/service/AiService.java
@@ -0,0 +1,17 @@
+package com.monji.ai_test.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AiService {
+
+    private final ChatClient chatClient;
+
+    public String getJoke(String topic) {
+        return chatClient.prompt().user("Give me a joke on the topic : " + topic)
+                .call().content();
+    }
+}
